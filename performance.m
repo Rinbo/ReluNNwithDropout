d64b2d8007ce709022e094bc270a466b44c6b9a2
Rinbo <robin.b@outlook.com>
2018-05-02 20:09:31 +0200

    Added missing sript - performance.m

---
+++ performance.m
@@ -0,0 +1,11 @@
+## Copyright (C) 2017 robin
+
+## Author: robin <robin@XPS15>
+## Created: 2017-06-06
+
+function acc = performance (H, Y)
+
+[dummy, predicted_digit] = max(H);
+acc = mean(predicted_digit' == Y);
+
+endfunction
